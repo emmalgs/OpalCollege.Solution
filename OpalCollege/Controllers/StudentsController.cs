01e2e7177d6ede3d2f1f48bc4c16d4fe24348a0d
Emma Gerigscott <elgerig@gmail.com>
2023-05-23 09:52:38 -0700

    use validation attributes to check user student input values
     Co-authored-by Thomas McDowell <tmcdowellddhs@yahoo.com>
    dquote> Co-authored-by Sarah Reimann <reimannsarah45@gmail.com>

--- OpalCollege/Controllers/StudentsController.cs
+++ OpalCollege/Controllers/StudentsController.cs
@@ -7,102 +7,111 @@ using System.Linq;
 
 namespace OpalCollege.Controllers
 {
-  public class StudentsController : Controller
-  {
-    private readonly OpalCollegeContext _db;
-
-    public StudentsController(OpalCollegeContext db)
+    public class StudentsController : Controller
     {
-      _db = db;
-    }
+        private readonly OpalCollegeContext _db;
 
-    public ActionResult Index()
-    {
-      List<Student> model = _db.Students.ToList();
-      return View(model);
-    }
+        public StudentsController(OpalCollegeContext db)
+        {
+            _db = db;
+        }
 
-    public ActionResult Create()
-    {
-      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
-      return View();
-    }
+        public ActionResult Index()
+        {
+            List<Student> model = _db.Students.ToList();
+            return View(model);
+        }
 
-    public ActionResult AddCourse (int id)
-    {
-      Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
-      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
-      return View(thisStudent);
-    }
+        public ActionResult Create()
+        {
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
+            return View();
+        }
 
-    [HttpPost]
-    public ActionResult AddCourse (Student student, int courseId)
-    {
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
+                return View();
+            }
+            else
+            {
+                _db.Students.Add(student);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult AddCourse(int id)
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
+            return View(thisStudent);
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(Student student, int courseId)
+        {
 #nullable enable
-      StudentCourse? joinEntity = _db.StudentCourses.FirstOrDefault(join => (join.CourseId == courseId && join.StudentId == student.StudentId));
+            StudentCourse? joinEntity = _db.StudentCourses.FirstOrDefault(join => (join.CourseId == courseId && join.StudentId == student.StudentId));
 #nullable disable
-      if (joinEntity == null && courseId != 0)
-      {
-        _db.StudentCourses.Add(new StudentCourse() { CourseId = courseId, StudentId = student.StudentId});
-        _db.SaveChanges();
-      }
-      return RedirectToAction("Details", new { id = student.StudentId });
-    }
-    
-    [HttpPost]
-    public ActionResult Create(Student student)
-    {
-      _db.Students.Add(student);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+            if (joinEntity == null && courseId != 0)
+            {
+                _db.StudentCourses.Add(new StudentCourse() { CourseId = courseId, StudentId = student.StudentId });
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Details", new { id = student.StudentId });
+        }
 
-    public ActionResult Details(int id)
-    {
-      Student foundStudent = _db.Students
-                                .Include(student => student.JoinEntities)
-                                .ThenInclude(join => join.Course)
-                                .FirstOrDefault(student => student.StudentId == id);
-      return View(foundStudent);
-    }
+        public ActionResult Details(int id)
+        {
+            Student foundStudent = _db.Students
+                                      .Include(student => student.JoinEntities)
+                                      .ThenInclude(join => join.Course)
+                                      .FirstOrDefault(student => student.StudentId == id);
+            return View(foundStudent);
+        }
 
-    public ActionResult Edit(int id)
-    {
-      Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
-      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
-      return View(thisStudent);
-    }
 
-    [HttpPost]
-    public ActionResult Edit(Student student)
-    {
-      _db.Students.Update(student);
-      _db.SaveChanges();
-      return RedirectToAction("Details", new { id = student.StudentId});
-    }
+        public ActionResult Edit(int id)
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Title");
+            return View(thisStudent);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            _db.Students.Update(student);
+            _db.SaveChanges();
+            return RedirectToAction("Details", new { id = student.StudentId });
+        }
 
         public ActionResult Delete(int id)
-    {
-      Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
-      return View(thisStudent);
-    }
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            return View(thisStudent);
+        }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
-      _db.Students.Remove(thisStudent);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            _db.Students.Remove(thisStudent);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-    [HttpPost]
-    public ActionResult DeleteJoin(int joinId)
-    {
-      StudentCourse joinEntry = _db.StudentCourses.FirstOrDefault(entry => entry.StudentCourseId == joinId);
-      _db.StudentCourses.Remove(joinEntry);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+        [HttpPost]
+        public ActionResult DeleteJoin(int joinId)
+        {
+            StudentCourse joinEntry = _db.StudentCourses.FirstOrDefault(entry => entry.StudentCourseId == joinId);
+            _db.StudentCourses.Remove(joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
--- OpalCollege/Models/Student.cs
+++ OpalCollege/Models/Student.cs
@@ -9,8 +9,12 @@ namespace OpalCollege.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0: yyyy-MM-dd}", ApplyFormatInEditMode=true)]
         public int StudentId { get; set; }
+        [Required(ErrorMessage ="Please add a name!!!!! YOU MUST!!!!")]
         public string Name { get; set; }
-        public DateTime EnrollDate { get; set; }
+        [Required(ErrorMessage = "Please select a date of enrollment, you coocoo caca insana")]
+        [DataType(DataType.DateTime, ErrorMessage = "PLEASE ENTER A DATE, PLEASE!!!!")]
+        [Range(typeof(DateTime), "2022-01-01", "2024-12-31", ErrorMessage = "ARe you insande thats snot ebven real")]
+        public DateTime? EnrollDate { get; set; }
         public List<StudentCourse> JoinEntities { get; }
     }
 }
